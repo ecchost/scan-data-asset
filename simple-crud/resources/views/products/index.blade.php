81894c7fe64a204b8abf7620175dd9edccfadb90
davegpakpahan <neet693@gmail.com>
2022-01-05 08:52:27 +0800

    Layouting Baru pada index dan show

--- simple-crud/resources/views/products/index.blade.php
+++ simple-crud/resources/views/products/index.blade.php
@@ -2,19 +2,16 @@
 
 @section('content')
 
-<br>
-<br>
 <div class="row">
-    <br>
+    <div class="pull-left">
+        <h2>Data RFID Card</h2>
+    </div>
     <br>
     <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Data RFID Card</h2>
-        </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('products.index') }}"> Refresh </a>
         </div>
-        <div class="pull-right">
+        <div class="pull-left">
             <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Data </a>
         </div>
         <div class="pull-right">
@@ -22,11 +19,12 @@
         </div>
     </div>
 </div>
+<br>
 <div class="mx-auto">
     <div class="">
         <form action="{{ route('products.index') }}" method="GET" role="search">
             <div class="input-group">
-                <input type="text" class="form-control mr-2" name="term" placeholder="Search Number ISPS/RFID or Name" id="term">
+                <input type="text" class="form-control mr-2" name="term" placeholder="Search Barcode" id="term">
                 <a href="{{ route('products.index') }}" class=" mt-1">
                 </a><br><br>
             </div>
@@ -42,41 +40,25 @@
 
 <table class="table table-bordered">
     <tr>
-        <th>No</th>
-        <th>RFID</th>
-        <th>Full Name</th>
-        <th>A</th>
-        <th>B</th>
-        <th>C</th>
-        <th>D</th>
-        <th>E</th>
-        <th>F</th>
-        <th>G</th>
-        <th>Instansion</th>
-        <th>ID Number</th>
-        <th>EXP Date</th>
-        <th>Image</th>
-        <th width="280px">Action</th>
-        <th>Status Kartu dan Status Vaksin</th>
+        <th>No.</th>
+        <th>Nama Barang Dan Divisi</th>
+        <th>Nama Barang Dan Divisi</th>
+        <th width="50px">Barcode</th>
+        <th width="50px">Action</th>
     </tr>
     @foreach ($products as $product)
     <tr>
         <td>{{ ++$i }}</td>
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->fullname }}</td>
-        <td>{{ $product->area_a }}</td>
-        <td>{{ $product->area_b }}</td>
-        <td>{{ $product->area_c }}</td>
-        <td>{{ $product->area_d }}</td>
-        <td>{{ $product->area_e }}</td>
-        <td>{{ $product->area_f }}</td>
-        <td>{{ $product->area_g }}</td>
-        <td>{{ $product->instansi }}</td>
-        <td>{{ $product->no_id }}</td>
-        <td>{{ $product->exp_date }}</td>
-        <td><img src="/image/{{ $product->image }}" width="100px"></td>
+        <td>{{ $product->fullname }} <br>{{ $product->divisi }}</td>
+        <td>{{ $product->fullname }} <br>{{ $product->divisi }}</td>
+        <td>{!! DNS2D::getBarcodeSVG($product->name, 'QRCODE',5,5) !!}</td>
         <td align="center">
             <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                <a class="btn btn-success" href="{{ route('products.show',$product->id) }}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                  </svg>
+                  </a><br><br>
                 <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                     </svg>
@@ -89,18 +71,6 @@
                 </svg>
                 </button>
             </form>
-        </td>
-        <td>
-            @if ($product->card_status == "SUDAH AMBIL")
-            <a class="btn btn-success">{{$product->card_status}}</a>
-            @else ($product->card_status == "BELUM AMBIL")
-            <a class="btn btn-warning">{{$product->card_status}}</a>
-            @endif<br><br>
-            @if ($product->no_vaksin_cov != "")
-            <a class="btn btn-success">{{$product->no_vaksin_cov}}</a>
-            @else ($product->no_vaksin_cov == "")
-            <a class="btn btn-warning">Belum Vaksin</a>
-            @endif
         </td>
     </tr>
     @endforeach
--- simple-crud/resources/views/products/show.blade.php
+++ simple-crud/resources/views/products/show.blade.php
@@ -1,11 +1,11 @@
 @extends('products.layout')
 
 @section('content')
-<link rel="stylesheet" type="text/css" href="css/create-admin.css">
-<br>
-<br>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="/css/create-admin.css">
 <div class="row">
-    <br><br>
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
             <h2> Show Data</h2><br>
@@ -18,74 +18,75 @@
 
 
 <div class="container">
-    <div class="row gutters">
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
-            <div class="card h-100">
-                <div class="card-body">
-                    <div class="account-settings">
-                        <div class="user-profile">
-                            <div class="user-avatar">
-                                <img src="/image/{{ $product->image }}" width="200px" alt="Maxwell Admin">
-                            </div><br>
-                            <h5 class="user-name">{{ $product->fullname }}</h5>
-                            <h6 class="user-email">{{ $product->instansi }}</h6>
-                        </div>
+    <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+        <div class="form-group">
+            <img src="/image/{{ $product->image }}" width="200px" alt="Maxwell Admin">
+            <br>
+            <div class="mb-3">{!! DNS2D::getBarcodeSVG($product->name, 'QRCODE',7,7) !!}</div>
+        </div>
+    </div>
+    <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+        <div class="row gutters">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mb-2 text-primary">ASSET DETAILS</h6>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                    <div class="form-group">
+                        <label>KATEGORI ASSET :</label>
+                        <h6>{{ $product->fullname }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>NILAI ASSET :</label>
+                        <h6>Rp {{ $product->nilai_asset }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>KONDISI AWAL ASSET :</label>
+                        <h6>{{ $product->first_condition }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>KONDISI SAAT INI :</label>
+                        <h6>{{ $product->last_condition }}</h6>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6">  
+                    <div class="form-group">
+                        <label>JENIS ASSET :</label>
+                        <h6>{{ $product->jenis_asset }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>LOKASI ASSET :</label>
+                        <h6>{{ $product->divisi }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>TANGGAL TERIMA ASSET :</label>
+                        <h6>{{ $product->exp_date }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>BATAS PAKAI :</label>
+                        <h6>{{ $product->btpakai }}</h6>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-6" style="margin-bottom: -50px">
+                    <div class="form-group">
+                        <h6 class="my-div2 text-white">Aktif</h6>
+                        <img src="/image/{{ $product->gb_asset }}" width="200px" alt="Gambar Asset" title="{{ $product->name }}">
+                        <h6 class="my-div text-white">{{ $product->fullname }}</h6>
+                    </div>
+                </div>
+                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-6">
+                    <div class="form-group">
+                        <label>RFID NUMBER :</label>
+                        <h6>{{ $product->name }}</h6>
+                    </div>
+                    <div class="form-group">
+                        <label>KETERANGAN TAMBAHAN :</label>
+                        {!! $product->keterangan_tambahan !!}
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
-            <div class="card h-100">
-                <div class="card-body">
-                <div class="row gutters">
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <h6 class="mb-2 text-primary">Personal Details</h6>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Full Name</label>
-                                    <input type="text" class="form-control" value="{{ $product->fullname }}" name="fullname" placeholder="Enter full name" readonly>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>RFID Number :</label>
-                                    <input type="text" class="form-control" value="{{ $product->name }}" name="name" placeholder="Enter RFID Number" readonly>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Area</label>
-                                    <input type="text" class="form-control" value="{{ $product->area }}" name="area" placeholder="Enter Area Location" readonly>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Instansi</label>
-                                    <input type="text" class="form-control" value="{{ $product->instansi }}" name="instansi" placeholder="Enter Instansi" readonly>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row gutters">
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <h6 class="mt-3 mb-2 text-primary">Unique Prof</h6>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>ID Number</label>
-                                    <input type="text" class="form-control" value="{{ $product->no_id }}" name="no_id" placeholder="Enter ID Number" readonly>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                <div class="form-group">
-                                    <label>Detail</label>
-                                    <input type="text" class="form-control" value="{{ $product->detail }}" name="detail" placeholder="Enter Details" readonly>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        {{-- <img src="/image/dekorasi.png" width="2000px" height="3000px" style="margin-bottom: -1000px" alt="Maxwell Admin"> --}}
+    </div>    
+
 </div>
 @endsection
