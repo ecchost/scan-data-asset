6987e078030179f537b9dccb8d24041838a40111
davegpakpahan <neet693@gmail.com>
2022-01-05 09:50:52 +0800

    entah kenapa layout edit error jd update lg

--- simple-crud/resources/views/products/newedit.blade.php
+++ simple-crud/resources/views/products/newedit.blade.php
@@ -79,7 +79,7 @@
                                 <h6 class="my-div text-white">{{ $product->fullname }}</h6>
                             </div>
                             </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12" style="margin-top: -100px">
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                 <div class="form-group">
                                     <label>KETERANGAN TAMBAHAN :</label>
                                     <input id="keterangan_tambahan" type="hidden" placeholder="{{ $product->keterangan_tambahan }}" value="{{ $product->keterangan_tambahan }}" name="keterangan_tambahan">
