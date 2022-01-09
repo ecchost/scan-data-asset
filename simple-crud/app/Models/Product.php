cebaf6602ee552c91aa34fff55c3d2c9108207d9
davegpakpahan <neet693@gmail.com>
2022-01-10 05:39:57 +0800

    Fixing Models

--- simple-crud/app/Models/Product.php
+++ simple-crud/app/Models/Product.php
@@ -9,7 +9,12 @@ class Product extends Model
 {
     use HasFactory;
     
-    protected $fillable = [
-        'name', 'fullname', 'area_a', 'area_b', 'area_c', 'area_d', 'area_e', 'area_f', 'area_g', 'instansi', 'no_id', 'image', 'exp_date', 'card_status', 'no_vaksin_cov', 'dosis_satu_cov', 'dosis_dua_cov'
-    ];
+    // protected $fillable = [
+    //     'name', 'fullname', 'area_a', 'area_b', 'area_c', 'area_d', 'area_e', 'area_f', 'area_g', 'instansi', 'no_id', 'image', 'exp_date', 'card_status', 'no_vaksin_cov', 'dosis_satu_cov', 'dosis_dua_cov'
+    // ];
+    protected $guarded = ['id'];
+
+    public function divisi(){
+        return $this->belongsTo('App\Models\Divisi');
+    }
 }
