d2091f08b7cf5a966228059a8e7a16e4d300ecd9
davegpakpahan <neet693@gmail.com>
2022-01-10 05:33:13 +0800

    Layout Index Dashboard ketinggalan

--- simple-crud/resources/views/dashboard/index.blade.php
+++ simple-crud/resources/views/dashboard/index.blade.php
@@ -80,69 +80,64 @@
         <div class="form-group">
             <img src="/image/{{ $product->image }}" width="200px" alt="Maxwell Admin">
             <br>
-            <div class="mb-3">{!! DNS2D::getBarcodeHTML($product->name, 'QRCODE',7,7) !!}</div>
+            <div class="mt-3">{!! DNS2D::getBarcodeSVG($product->name, 'QRCODE',6,6) !!}</div>
         </div>
     </div>
     <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
         <div class="row gutters">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 class="mb-2 text-primary">ASSET DETAILS</h6>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <h6 class="mb-2 text-primary">ASSET DETAILS</h6>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+                <div class="form-group">
+                    <h6><label>DIVISI :</label></h6>
+                    {{ $product->divisi->nama_divisi }}
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-                    <div class="form-group">
-                        <label>KATEGORI ASSET :</label>
-                        <h6>{{ $product->fullname }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>NILAI ASSET :</label>
-                        <h6>Rp {{ $product->nilai_asset }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>KONDISI AWAL ASSET :</label>
-                        <h6>{{ $product->first_condition }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>KONDISI SAAT INI :</label>
-                        <h6>{{ $product->last_condition }}</h6>
-                    </div>
+                <div class="form-group">
+                    <h6><label>JABATAN :</label></h6>
+                    {{ $product->jabatan }}
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6">  
-                    <div class="form-group">
-                        <label>JENIS ASSET :</label>
-                        <h6>{{ $product->jenis_asset }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>LOKASI ASSET :</label>
-                        <h6>{{ $product->divisi }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>TANGGAL TERIMA ASSET :</label>
-                        <h6>{{ $product->exp_date }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>BATAS PAKAI :</label>
-                        <h6>{{ $product->btpakai }}</h6>
-                    </div>
+                <div class="form-group">
+                    <h6><label>USER :</label></h6>
+                    {{ $product->pengguna }}
                 </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-6" style="margin-bottom: -50px">
-                    <div class="form-group">
-                        <h6 class="my-div2 text-white">Aktif</h6>
-                        <img src="/image/{{ $product->gb_asset }}" width="200px" alt="Gambar Asset" title="{{ $product->name }}">
-                        <h6 class="my-div text-white">{{ $product->fullname }}</h6>
-                    </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6">
+                <div class="form-group">
+                    <h6><label>COMPUTER TYPE :</label></h6>
+                    {{ $product->fullname }}
                 </div>
-                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-6">
-                    <div class="form-group">
-                        <label>RFID NUMBER :</label>
-                        <h6>{{ $product->name }}</h6>
-                    </div>
-                    <div class="form-group">
-                        <label>KETERANGAN TAMBAHAN :</label>
-                        {!! $product->keterangan_tambahan !!}
-                    </div>
+                <div class="form-group">
+                    <h6><label>KONDISI ASSET :</label></h6>
+                    {{ $product->asset_condition }}
+                </div>
+                 <div class="form-group">
+                    <h6><label>BARCODE LINK :</label></h6>
+                    {{ $product->name }}
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-6" style="margin-bottom: -50px">
+                <div class="form-group">
+                    <h6 class="my-div2 text-white">{{ $product->asset_condition }}</h6>
+                    <img src="/image/{{ $product->gb_asset }}" width="200px" alt="Gambar Asset" title="{{ $product->name }}">
+                    <h6 class="my-div text-white">{{ $product->fullname }}</h6>
+                </div>
+            </div>
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-6" style="margin-top: -50px">
+                <div class="form-group">
+                    <h6><label>KETERANGAN TAMBAHAN :</label></h6>
+                    <label style="margin-left: 20px"> SN :</label> {{ $product->sn }} <br>
+                    <label style="margin-left: 20px"> OS :</label> {{ $product->os }} <br>
+                    <label style="margin-left: 20px"> IP :</label> {{ $product->ip }}
+                    <label style="margin-left: 20px"> MAC ADDRESS :</label> {{ $product->mac_address }} <br>
+                    <label style="margin-left: 20px"> HDD :</label> {{ $product->hdd }} 
+                    <label style="margin-left: 20px"> SSD :</label> {{ $product->ssd }} <br>
+                    <label style="margin-left: 20px"> MONITOR :</label> {{ $product->monitor }} <br>
+                    <label style="margin-left: 20px"> PRINTER :</label> {{ $product->printer }} <br>
                 </div>
             </div>
         </div>
+    </div>
         {{-- <img src="/image/dekorasi.png" width="2000px" height="3000px" style="margin-bottom: -1000px" alt="Maxwell Admin"> --}}
     </div>    
 
