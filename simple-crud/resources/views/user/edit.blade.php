022e32e6116c5f47f6fb0bd71a32395af8030fb2
unknown <neet693@gmail.com>
2022-01-31 07:33:26 +0700

    Fixing Logo di Tambah USer dan Edit  USer

--- simple-crud/resources/views/user/edit.blade.php
+++ simple-crud/resources/views/user/edit.blade.php
@@ -1,51 +1,51 @@
 @extends('products.layout')
 
 @section('content')
-<div class="container" background="white">
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <img src="https://upload.wikimedia.org/wikipedia/id/0/0a/Pelindo_IV.svg" width="400">
-        </x-slot>
+    <div class="container" background="white">
+        <x-jet-authentication-card>
+            <x-slot name="logo">
+                <img src="/image/logo-pelindo.png" width="400">
+            </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+            <x-jet-validation-errors class="mb-4" />
 
-        <form action="{{route('user.update', $item->id)}}" method="POST" enctype="multipart/form-data">
-            @method('PUT')
-            @csrf
-            <div class="container" align="center">
-                <h4>Edit</h4>
-            </div>
-            <br>
-
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $item->name }}" required
-                    autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ $item->email }}"
-                    required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Access') }}" />
-                <div class="input-group mb-3">
-                    <select class="form-select" id="user_level" name="user_level" value="{{ $item->user_level }}">
-                        <option value="user">User</option>
-                        <option value="admin">Admin</option>
-                    </select>
+            <form action="{{ route('user.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+                @method('PUT')
+                @csrf
+                <div class="container" align="center">
+                    <h4>Edit</h4>
                 </div>
-            </div>
+                <br>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button class="ml-4">
-                    <a>Edit</a>
-                </x-jet-button>
-            </div>
+                <div>
+                    <x-jet-label for="name" value="{{ __('Name') }}" />
+                    <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $item->name }}"
+                        required autofocus autocomplete="name" />
+                </div>
 
-        </form>
-    </x-jet-authentication-card>
-</div>
+                <div class="mt-4">
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ $item->email }}"
+                        required />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="password_confirmation" value="{{ __('Access') }}" />
+                    <div class="input-group mb-3">
+                        <select class="form-select" id="user_level" name="user_level" value="{{ $item->user_level }}">
+                            <option value="user">User</option>
+                            <option value="admin">Admin</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-jet-button class="ml-4">
+                        <a>Edit</a>
+                    </x-jet-button>
+                </div>
+
+            </form>
+        </x-jet-authentication-card>
+    </div>
 @endsection
